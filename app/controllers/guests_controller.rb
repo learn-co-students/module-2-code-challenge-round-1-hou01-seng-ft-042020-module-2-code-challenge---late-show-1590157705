bea63158eaf30363815b335f031597dc3ef942b8
Eric Johnson <johnson.n.eric@gmail.com>
2020-05-22 11:00:53 -0500

    I legit typed HMTL

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,16 @@
 class GuestsController < ApplicationController
 
+  before_action :current_guest, only: [:show]
+
   def index
     @guests = Guest.all
+  end
+
+  def show
+    # byebug
+  end
+
+  def current_guest
+    @guest = Guest.find(params[:id])
   end
 end
