a794def84a2ae9425dc3499a5ae2a74d701d5495
lbirts <birtslauren@gmail.com>
2020-05-22 10:58:55 -0500

    fin

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,18 @@
 class GuestsController < ApplicationController
 
+  before_action :current_guest, only: [:show]
+
   def index
     @guests = Guest.all
+  end
+
+  def show
+    @appearances_order = @guest.appearances.order("rating DESC")
+  end
+
+  private
+
+  def current_guest
+    @guest = Guest.find(params[:id])
   end
 end
