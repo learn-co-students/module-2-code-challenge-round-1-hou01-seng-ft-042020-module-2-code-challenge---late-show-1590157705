77661827c315eadcac68314275993e43326161f9
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-05-22 10:59:56 -0500

    Krysta done

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,12 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def show
+    @guest = Guest.find(params[:id])
+    # temp = @guest.episodes
+    # temp[1].appearances[1].rating
+    # byebug
+  end
+
 end
