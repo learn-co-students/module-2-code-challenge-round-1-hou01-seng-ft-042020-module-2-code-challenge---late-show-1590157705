cc1f9963d2f3be19e4bac12f4e946f35f895efa4
Stephanie Cheney <steph@scheneyphotography.com>
2020-05-22 11:00:00 -0500

    done

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,5 +3,5 @@ class Appearance < ApplicationRecord
     belongs_to :episode
 
     validates :rating, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 5}
- 
+    validates :guest, uniqueness: true 
 end
