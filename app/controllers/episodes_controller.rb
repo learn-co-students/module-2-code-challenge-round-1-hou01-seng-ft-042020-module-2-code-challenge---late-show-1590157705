2107abfd6badeafb6d17c36718547315028e9532
eiphyllis <phyllisgetcity@gmail.com>
2020-05-22 11:02:59 -0500

    yikes

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,23 @@
 class EpisodesController < ApplicationController
 
+  before_action :current_episode, only: [:show]
+  # before_action :new_episode, only [:create, :new]
   def index
     @episodes = Episode.all
   end
+
+  def show
+    @guests = Guest.all
+  end
+
+  def current_episode
+    @episode = Episode.find(params[:id])
+
+  end 
+
+  # def new_episode
+  #   @episode = Episode.new
+  # end
+
+
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,16 @@
 class GuestsController < ApplicationController
-
+before_action :current_guest, only: [:show]
   def index
     @guests = Guest.all
   end
+
+  def show
+    @episodes = Episode.all
+  end
+
+  def current_guest
+    @guest = Guest.find(params[:id])
+
+  end 
+  
 end
